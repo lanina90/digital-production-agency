7356399e271c40e166f085880110499a05ea9251
Kateryna Laina <boba4ok@gmail.com>
2024-01-02 15:41:45 +0200

    feature: testimonials section

---
+++ app/components/Testimonials/TestimonialCard/TestimonialCard.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React from 'react';
+import Image from "next/image";
+import {useMedia} from "@/hooks/useMedia";
+import styles from './styles.module.scss'
+
+const TestimonialCard = ({title, text, name, position, photo}
+                           : { title: string, text: string, name: string, position: string, photo: string }) => {
+  const {deviceType} = useMedia()
+
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.testimonial}>
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </div>
+      <div className={styles.info}>
+        <div className={styles.person}>
+          <div className={styles.photo}>
+            <Image  src={photo} alt={name} width={40} height={40} />
+          </div>
+          <div>
+            <p className={styles.name}>{name}</p>
+            <p className={styles.position}>{position}</p>
+          </div>
+        </div>
+        <div className={styles.name}>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
