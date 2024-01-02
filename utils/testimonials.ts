5d50020cad5fb1509c5de3ec33d95b01ce734acb
Kateryna Laina <boba4ok@gmail.com>
2024-01-02 15:45:00 +0200

    feature: testimonials section

--- utils/testimonials.ts
+++ utils/testimonials.ts
@@ -2,7 +2,7 @@ export const testimonials = [
   {
     id: 1,
     title: "SquareUp has been Instrumental in Transforming our Online Presence. ",
-    text: "Their team's expertise in web development and design resulted in a visually stunning and user-friendly e-commerce platform. Our online sales have skyrocketed, and we couldn't be happier.",
+    text: "Their team&apos;s expertise in web development and design resulted in a visually stunning and user-friendly e-commerce platform. Our online sales have skyrocketed, and we couldn&apos;t be happier.",
     name: "John Smith",
     position: "CEO of Chic Boutique",
     photo: "/images/testimonials/john.jpg"
@@ -18,7 +18,7 @@ export const testimonials = [
   {
     id: 3,
     title: "SquareUp developed a comprehensive booking and reservation system for our event management company",
-    text: "Their attention to detail and commitment to delivering a user-friendly platform was evident throughout the project. The system has streamlined our operations and enhanced our clients' event experiences.",
+    text: "Their attention to detail and commitment to delivering a user-friendly platform was evident throughout the project. The system has streamlined our operations and enhanced our clients&apos; event experiences.",
     name: "Mark Thompson",
     position: "CEO of EventMasters",
     photo: "/images/testimonials/mark.jpg"
@@ -26,7 +26,7 @@ export const testimonials = [
   {
     id: 4,
     title: "ProTech Solutions turned to SquareUp to automate our workflow",
-    text: "They delivered an exceptional custom software solution. The system has significantly increased our productivity and reduced manual errors. SquareUp's expertise and professionalism have made them a trusted technology partner.",
+    text: "They delivered an exceptional custom software solution. The system has significantly increased our productivity and reduced manual errors. SquareUp&apos;s expertise and professionalism have made them a trusted technology partner.",
     name: "Laura Adams",
     position: "COO of ProTech Solutions.",
     photo: "/images/testimonials/laura.jpg"
@@ -34,7 +34,7 @@ export const testimonials = [
   {
     id: 5,
     title: "SquareUp designed and developed a captivating web portal for showcasing our real estate listings.",
-    text: "The platform is visually appealing and easy to navigate, allowing potential buyers to find their dream homes effortlessly. SquareUp's expertise in the real estate industry is unmatched.",
+    text: "The platform is visually appealing and easy to navigate, allowing potential buyers to find their dream homes effortlessly. SquareUp&apos;s expertise in the real estate industry is unmatched.",
     name: "Michael Anderson",
     position: "Founder of Dream Homes Realty.",
     photo: "/images/testimonials/michael.jpg"
@@ -42,7 +42,7 @@ export const testimonials = [
   {
     id: 6,
     title: "FitLife Tracker wanted a mobile app that tracked fitness activities and provided personalized workout plans.",
-    text: "SquareUp's team developed an intuitive and feature-rich app that has helped our users stay motivated and achieve their fitness goals. We highly recommend SquareUp for any health and fitness app development needs.",
+    text: "SquareUp&apos;s team developed an intuitive and feature-rich app that has helped our users stay motivated and achieve their fitness goals. We highly recommend SquareUp for any health and fitness app development needs.",
     name: "Emily Turner",
     position: "CEO of FitLife Tracker",
     photo: "/images/testimonials/emily.jpg"
