04ed5e0252927718d62047e73b559982256c5818
Kateryna Laina <boba4ok@gmail.com>
2024-01-02 15:55:30 +0200

    feature: testimonials section

--- app/components/Testimonials/Testimonials.tsx
+++ app/components/Testimonials/Testimonials.tsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
     <section className={styles.testimonials}>
       <div className={styles.intro}>
         <h2>What our Clients say About us</h2>
-        <p>At SquareUp, we take pride in delivering exceptional digital products and services that drive success for our clients. Here's what some of our satisfied clients have to say about their experience working with us</p>
+        <p>At SquareUp, we take pride in delivering exceptional digital products and services that drive success for our clients. Here&apos;s what some of our satisfied clients have to say about their experience working with us</p>
       </div>
       <div className={styles.list}>
         {testimonials.map(testimonial =>
