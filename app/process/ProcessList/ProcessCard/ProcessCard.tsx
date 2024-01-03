76a46d2529a62d68472cd61de782032ff85052eb
Kateryna Laina <boba4ok@gmail.com>
2024-01-03 17:58:46 +0200

    feature: process page

---
+++ app/process/ProcessList/ProcessCard/ProcessCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles  from  './ProcessCard.module.scss'
+
+const ProcessCard = ({index, item: {
+  name, description, id
+}}: {index:number, item: {
+    name:string, description:string, id:number
+  }}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.title}>
+        <div className={styles.number}>0{index+1}</div>
+        <div className={styles.text}>
+          {name}
+        </div>
+      </div>
+      <p className={styles.description}>{description}</p>
+    </div>
+  );
+};
+
+export default ProcessCard;
